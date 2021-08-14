576fc9ed72fe007680587c51d7c04c7821b57a7f
myifengs <myifengs@gmail.com>
2021-08-14 22:22:37 +0800

    gateway

---
+++ swan-gateway-app/src/main/java/io/github/myifeng/swan/gateway/GatewayApplication.java
@@ -0,0 +1,13 @@
+package io.github.myifeng.swan.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.WebApplicationType;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+
+@SpringBootApplication
+public class GatewayApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(GatewayApplication.class, args);
+	}
+}
