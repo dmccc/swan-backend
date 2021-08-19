b817011ac7fe306165e0d1184a0894a4ddf8cab6
liqingfeng <liqingfeng@jianguodata.com>
2021-08-19 13:36:41 +0800

    FIX: Unit Test

--- swan-appendix-app/src/test/java/io/github/myifeng/swan/appendix/test/AppendixApiTest.java
+++ swan-appendix-app/src/test/java/io/github/myifeng/swan/appendix/test/AppendixApiTest.java
@@ -1,17 +1,7 @@
 package io.github.myifeng.swan.appendix.test;
 
-import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.extension.ExtendWith;
-import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.ContextConfiguration;
-import org.springframework.test.context.junit.jupiter.SpringExtension;
 
-@SuppressWarnings("unused")
-@ExtendWith(SpringExtension.class)
 @SpringBootTest
-@AutoConfigureMockMvc
-@DisplayName("Appendix API 测试")
-@ContextConfiguration
-class AppendixApiTest {
+public class AppendixApiTest {
 }
--- swan-auth-app/src/test/java/io/github/myifeng/swan/auth/test/OAuthApplicationTest.java
+++ swan-auth-app/src/test/java/io/github/myifeng/swan/auth/test/OAuthApplicationTest.java
@@ -1,5 +1,7 @@
 package io.github.myifeng.swan.auth.test;
 
+import org.springframework.boot.test.context.SpringBootTest;
+
 @SpringBootTest
 public class OAuthApplicationTest {
 }
