4c959d4d883a2809d6c75347e15de4e5a6544dd9
myifengs <myifengs@gmail.com>
2021-08-21 09:40:31 +0800

    oauth config

---
+++ swan-auth-app/src/main/java/io/github/myifeng/swan/auth/config/AuthorizationServerConfiguration.java
@@ -0,0 +1,11 @@
+package io.github.myifeng.swan.auth.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.oauth2.config.annotation.web.configuration.AuthorizationServerConfigurerAdapter;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
+
+
+@Configuration
+@EnableAuthorizationServer
+public class AuthorizationServerConfiguration extends AuthorizationServerConfigurerAdapter {
+}
---
+++ swan-auth-app/src/main/java/io/github/myifeng/swan/auth/config/SecurityConfiguration.java
@@ -0,0 +1,8 @@
+package io.github.myifeng.swan.auth.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@Configuration
+public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
+}
