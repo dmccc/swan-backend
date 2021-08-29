e01411d9a8de25e0ed199f6b9915c0e7b2225b34
myifengs <myifengs@gmail.com>
2021-08-29 10:01:36 +0800

    update ci & router demo -> hello

--- swan-demo-app/src/main/java/io/github/myifeng/swan/demo/controller/DemoController.java
+++ swan-demo-app/src/main/java/io/github/myifeng/swan/demo/controller/DemoController.java
@@ -12,7 +12,7 @@ import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("/demo")
+@RequestMapping("/hello")
 public class DemoController {
 
     private final DemoDao dao;
