c7b8f096181ec78fc7a85dbe63c4c8b241a914ae
myifengs <myifengs@gmail.com>
2021-08-29 17:24:00 +0800

    FIX: BeanCurrentlyInCreationException

--- swan-auth-app/src/main/java/io/github/myifeng/swan/auth/config/AuthorizationServerConfiguration.java
+++ swan-auth-app/src/main/java/io/github/myifeng/swan/auth/config/AuthorizationServerConfiguration.java
@@ -2,10 +2,8 @@ package io.github.myifeng.swan.auth.config;
 
 import io.github.myifeng.swan.auth.service.SwanUserDetailsService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.oauth2.config.annotation.configurers.ClientDetailsServiceConfigurer;
 import org.springframework.security.oauth2.config.annotation.web.configuration.AuthorizationServerConfigurerAdapter;
@@ -39,12 +37,6 @@ public class AuthorizationServerConfiguration extends AuthorizationServerConfigu
 
 	@Autowired
 	private TokenStore jwtTokenStore;
-
-	@Bean
-	public PasswordEncoder passwordEncoder() {
-		return new BCryptPasswordEncoder();
-	}
-
 
 	@Override
 	public void configure(AuthorizationServerEndpointsConfigurer endpoints) throws Exception {
--- swan-auth-app/src/main/java/io/github/myifeng/swan/auth/config/SecurityConfiguration.java
+++ swan-auth-app/src/main/java/io/github/myifeng/swan/auth/config/SecurityConfiguration.java
@@ -8,16 +8,19 @@ import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Configuration
 public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 
 	@Autowired
-	private PasswordEncoder passwordEncoder;
-
-	@Autowired
 	private SwanUserDetailsService swanUserDetailsService;
+
+	@Bean
+	public static PasswordEncoder passwordEncoder() {
+		return new BCryptPasswordEncoder();
+	}
 
 	@Bean
 	@Override
@@ -42,7 +45,7 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 	@Override
 	protected void configure(AuthenticationManagerBuilder auth) throws Exception{
 		auth.userDetailsService(swanUserDetailsService)
-				.passwordEncoder(passwordEncoder);
+				.passwordEncoder(passwordEncoder());
 	}
 
 }
